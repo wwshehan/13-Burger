340434c7ce027b50bfeb33b0f09edb2fd44cdb8f
Warren Shehan <wwshehan@gmail.com>
2020-03-13 10:03:08 -0400

    fixing heroku

--- server.js
+++ server.js
@@ -15,13 +15,18 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
+let connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
 });
+};
 
 connection.connect(function (err) {
   if (err) {
